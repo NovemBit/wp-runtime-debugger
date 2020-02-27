3d0c272000a8c5206423f6ad1ae9530edf406af6
diazoxide <aaron.yor@gmail.com>
2020-02-27 13:04:32 +0400

    Create Plugin

---
+++ wp-runtime-debugger.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name:       WP Runtime Debugger
+ * Version:           3.2.4
+ * Author:            Novembit
+ * Text Domain:       novembit
+ */
+
+register_activation_hook( __FILE__, function () {
+
+	$source = __DIR__ . '/mu-plugin/mu.php';
+	$target = WPMU_PLUGIN_DIR . '/wp-runtime-debugger.php';
+
+	if ( ! file_exists( WPMU_PLUGIN_DIR ) || ! is_dir( WPMU_PLUGIN_DIR ) ) {
+		mkdir( WPMU_PLUGIN_DIR );
+	}
+
+	if ( ! copy( $source, $target ) ) {
+		add_action(
+			'admin_notices',
+			function () {
+				?>
+                <div class="notice notice-success is-dismissible">
+                    <p><?php _e( 'Can\'t install mu-plugin file!', 'novembit' ); ?></p>
+                </div>
+				<?php
+			}
+		);
+	}
+
+} );
+register_deactivation_hook( __FILE__, function () {
+	$target = WPMU_PLUGIN_DIR . '/wp-runtime-debugger.php';
+
+	if ( unlink( $target ) ) {
+		return true;
+	}
+
+	return false;
+} );
+
+
